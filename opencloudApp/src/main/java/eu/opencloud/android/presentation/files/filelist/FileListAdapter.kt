e30582353e5aa5c80109243e19850addfce10588
Phillip9933 <phil@rogers.ltd>
2026-09-05 02:18:46 +0900

    fix: restore FileListAdapter color import

--- opencloudApp/src/main/java/eu/opencloud/android/presentation/files/filelist/FileListAdapter.kt
+++ opencloudApp/src/main/java/eu/opencloud/android/presentation/files/filelist/FileListAdapter.kt
@@ -32,6 +32,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
